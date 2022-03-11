56b9054f4ef2bdc11b2778e552c98204e2770121
Davidcharlie381 <davidcharleswisdom@gmail.com>
2022-03-11 15:15:39 -0800

    Test

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - base ten
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int base;
+
+	for (base = '0'; base <= '9'; base++)
+	{
+		putchar(base);
+	}
+	putchar('\n');
+}
