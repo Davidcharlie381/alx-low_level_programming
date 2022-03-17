1c84395f0a157c2c4317fc1dab3b09e4bb01366a
Davidcharlie381 <davidcharleswisdom@gmail.com>
2022-03-17 12:58:22 -0700

    do

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,53 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
-* print_times_table - Prints the n times table
-*
-* @n: number times table (0 < n <= 15)
-*
-* Return: no return
-*/
+ * print_times_table - prints it
+ *
+ * @n: arg
+ *
+ * Return: 0 or 1
+ */
 void print_times_table(int n)
 {
-	int a, b, op;
+	int x = 0, y, z;
 
-	if (n >= 0 && n <= 15)
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-	for (a = 0; a <= n; a++)
-	{
-	_putchar(48);
-	for (b = 1; b <= n; b++)
-	{
-		op = a * b;
-		_putchar(44);
-		_putchar(32);
-		if (op <= 9)
+		for (y = 0; y <= n; y++)
 		{
-		_putchar(32);
-		_putchar(32);
-		_putchar(op + 48);
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		else if (op <= 99)
-		{
-		_putchar(32);
-		_putchar((op / 10) + 48);
-		_putchar((op % 10) + 48);
-		}
-		else
-		{
-		_putchar(((op / 100) % 10) + 48);
-		_putchar(((op / 10) % 10) + 48);
-		_putchar((op % 10) + 48);
-		}
-	}
-	_putchar('\n');
-	}
+		_putchar('\n');
+		x++;
 	}
 }
